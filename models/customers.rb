2d3638273cd1cb1fd60b3b42103c6c54e1cd23e6
user <user@users-MacBook-Pro-8.local>
2019-11-22 17:32:28 +0000

    Customer save method (tested in pry and in db)

--- models/customers.rb
+++ models/customers.rb
@@ -1,10 +1,22 @@
+require_relative("../db/sqlrunner")
+
 class Customer
 
+  attr_accessor :name, :funds
+  attr_reader :id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
     @funds = options['funds'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO customers(name, funds)
+    VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    customer = SqlRunner.run(sql, values)[0]
+    @id = customer['id'].to_i()
   end
 
 end
