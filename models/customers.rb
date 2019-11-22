64764aa4e738e46a29c509f625cff10068215f97
user <user@users-MacBook-Pro-8.local>
2019-11-22 19:34:31 +0000

    Customer.films added (tested)

--- models/customers.rb
+++ models/customers.rb
@@ -30,4 +30,16 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
+  def
+    films
+      sql ="SELECT films.*
+    FROM films
+    INNER JOIN tickets
+    ON tickets.film_id = films.id WHERE customer_id = $1";
+      values = [@id]
+      films = SqlRunner.run(sql, values)
+      result = films.map { |film| Film.new(film) }
+      return result
+  end
+
 end
