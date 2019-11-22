03b08d54c6e4d999063d1d75bc96946a34d70732
user <user@users-MacBook-Pro-8.local>
2019-11-22 19:02:19 +0000

    Ticket.update added (tested)

--- models/tickets.rb
+++ models/tickets.rb
@@ -24,4 +24,10 @@ class Ticket
     SqlRunner.run(sql)
   end
 
+  def update()
+    sql = "UPDATE tickets SET (customer_id, film_id) = ($1, $2) WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
 end
