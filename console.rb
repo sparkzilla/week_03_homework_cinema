5a5033961c19ea5ebd850d5db66dfa2edae2046d
user <user@users-MacBook-Pro-8.local>
2019-11-22 17:14:59 +0000

    Create console.rb with minimum data set (untested)

---
+++ console.rb
@@ -0,0 +1,24 @@
+require_relative( 'models/customers' )
+require_relative( 'models/films' )
+require_relative( 'models/tickets' )
+
+require( 'pry-byebug' )
+
+# Tickets.delete_all()
+# Films.delete_all()
+# Customer.delete_all()
+
+customer1 = Customer.new({ 'name' => 'Mark', 'funds' => 50 })
+customer1.save()
+
+
+film1 = Film.new({ 'title' => 'Blade Runner', 'price' => 10})
+location1.save()
+
+
+ticket1 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket1.save()
+
+
+binding.pry
+nil
