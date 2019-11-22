39fa066e69284b857b8b0226eddb7f7f1ce0f5ef
user <user@users-MacBook-Pro-8.local>
2019-11-22 18:58:21 +0000

    Film.update added (tested)

--- models/films.rb
+++ models/films.rb
@@ -24,5 +24,10 @@ class Film
     SqlRunner.run(sql)
   end
 
+  def update()
+    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
 
 end
